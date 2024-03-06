1b2564dc3c24e1181d0025d8b2256b26b5b36ec3
Doan Trong Hieu <harrisondoan2810@gmail.com>
2024-03-06 08:47:02 +0700

    Learn Golang

--- EngineerPro/cmd/main.go
+++ EngineerPro/cmd/main.go
@@ -1,2 +1,73 @@
 package cmd
 
+type Config struct {
+	DatabasePassword string
+}
+
+type UserDataAccessor struct {
+	Config Config
+}
+
+type OrderDataAccessor struct {
+	Config Config
+}
+
+type ProductDataAccessor struct {
+	Config Config
+}
+
+type PostDataAccessor struct {
+	Config Config
+}
+
+type SocialLogic struct {
+	UserDataAccessor UserDataAccessor
+	PostDataAccessor PostDataAccessor
+}
+
+func NewSocialLogic(
+	UserDataAccessor UserDataAccessor,
+	PostDataAccessor PostDataAccessor,
+) *SocialLogic {
+	return &SocialLogic{
+		UserDataAccessor: UserDataAccessor,
+		PostDataAccessor: PostDataAccessor,
+	}
+}
+
+type MarketplaceLogic struct {
+	UserDataAccessor    UserDataAccessor
+	OrderDataAccessor   OrderDataAccessor
+	ProductDataAccessor ProductDataAccessor
+}
+
+func NewMarketplaceLogic(
+	UserDataAccessor UserDataAccessor,
+	OrderDataAccessor OrderDataAccessor,
+	ProductDataAccessor ProductDataAccessor,
+) *MarketplaceLogic {
+	return &MarketplaceLogic{
+		UserDataAccessor:    UserDataAccessor,
+		OrderDataAccessor:   OrderDataAccessor,
+		ProductDataAccessor: ProductDataAccessor,
+	}
+}
+
+type Server struct {
+	SocialLogic      SocialLogic
+	MarketplaceLogic MarketplaceLogic
+}
+
+func NewServer(
+	SocialLogic SocialLogic,
+	MarketplaceLogic MarketplaceLogic,
+) *Server {
+	return &Server{
+		SocialLogic:      SocialLogic,
+		MarketplaceLogic: MarketplaceLogic,
+	}
+}
+
+func main() {
+	// 
+}
